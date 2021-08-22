a2b516fb09728afcb49b855ea0e6e851ce599a4d
Ibrahim Boubakri <ibrahim.boubakri@ensi-uma.tn>
2021-08-22 09:13:27 +0100

    v5.1

--- app/Http/Controllers/abscenceController.php
+++ app/Http/Controllers/abscenceController.php
@@ -3,7 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use App\Models\classe;
+use App\Models\Classe;
 use App\Models\eleve;
 use App\Models\abscence;
 use App\Models\aff_enseignant;
@@ -15,7 +15,7 @@ use Illuminate\Support\Facades\Auth;
 class abscenceController extends Controller
 {
     public function abscenceView($id){
-        $classe=["classe"=>classe::where("id",$id)->first()];
+        $classe=["classe"=>Classe::where("id",$id)->first()];
         $eleves_aff=affc_eleve::where('classe_id',$id)->get();
         $seances = DB::table('seances')->where('id_enseignant', Auth::user()->id_enseignant)->where('id_classe', $id)->join('matieres', 'matieres.id', '=', 'seances.id_matiere')->get();
         return view('backend.partie_Ens.abscenceView',compact('classe','eleves_aff','seances'));
--- database/factories/seanceFactory.php
+++ database/factories/seanceFactory.php
@@ -31,7 +31,7 @@ class seanceFactory extends Factory
     public function definition()
     {
 
-        $hour = rand(8,18);
+        $hour = rand(8,17);
         $minits = rand(0,1);
         if ($hour < 10){
             $time_debut = '0'.$hour.':'.($minits * 3).'0:00';
@@ -40,21 +40,21 @@ class seanceFactory extends Factory
         }
 
 
-            $hour2 = rand(8,18);
-            $minits2 = rand(0,1);
-            while(($hour2 <= $hour) || (($hour2 == $hour) && ($minits2 <= $minits))){
+
+            do {
 
                 $hour2 = rand(8,18);
                 $minits2 = rand(0,1);
-            }
-            
+
+            } while(($hour2 < $hour) || (($hour2 == $hour) && ($minits2 <= $minits)));
+
             if ($hour2 < 10){
                 $time_fin = '0'.$hour2.':'.($minits2 * 3).'0:00';
             } else {
                 $time_fin = $hour2.':'.($minits2 * 3).'0:00';
             }
-        
-       
+
+
         $enseignants = enseignant::get()->count();
         $classes = Classe::get()->count();
         $matieres = Matiere::get()->count();

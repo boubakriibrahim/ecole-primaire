9ec23cc7892f26f52adb3532b24705802ebd08ea
Ibrahim Boubakri <ibrahim.boubakri@ensi-uma.tn>
2021-08-22 12:48:31 +0100

    v5.3

--- resources/views/admin/admin_master.blade.php
+++ resources/views/admin/admin_master.blade.php
@@ -155,10 +155,10 @@
 
     <link rel="stylesheet" href="{{ asset('css/toaster.css') }}">
 
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+{{--     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
 
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
-<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script> --}}
 
     {{-- time picker --}}
     <link rel="stylesheet" href="{{ asset('css/timepicker-min.css')}}">
--- routes/web.php
+++ routes/web.php
@@ -16,6 +16,7 @@ use App\Http\Controllers\ClasseController;
 use App\Http\Controllers\EmploiController;
 use App\Http\Controllers\SeanceController;
 use App\Http\Controllers\HomeController;
+use App\Http\Controllers\noteController;
 use App\Http\Controllers\Auth\LoginController;
 
 /*
@@ -184,25 +185,25 @@ Route::prefix('list')->group(function(){
 
 });
 Route::prefix('abscence')->group(function(){
-    Route::get('/abscence/{id}', [App\Http\controllers\abscenceController::class, 'abscenceView'])->name('abscence.view');
+    Route::get('/abscence/{id}', [abscenceController::class, 'abscenceView'])->name('abscence.view');
 
-    Route::get('/creeClasse/{id}', [App\Http\controllers\listClassController::class, 'listView'])->name('list.view');
-    Route::get('/checkList/{id}', [App\Http\controllers\listClassController::class, 'listCheck'])->name('list.check');
+    Route::get('/creeClasse/{id}', [listClassController::class, 'listView'])->name('list.view');
+    Route::get('/checkList/{id}', [listClassController::class, 'listCheck'])->name('list.check');
 
-    Route::post('/store/{id}', [App\Http\controllers\abscenceController::class, 'abscenceStore'])->name('abscence.store');
+    Route::post('/store/{id}', [abscenceController::class, 'abscenceStore'])->name('abscence.store');
 
-    Route::get('/edit/{id}', [App\Http\controllers\listClassController::class, 'editList'])->name('list.edit');
+    Route::get('/edit/{id}', [listClassController::class, 'editList'])->name('list.edit');
 
-    Route::post('/modifier/{id}', [App\Http\controllers\listClassController::class, 'updateList'])->name('list.miseajour');
+    Route::post('/modifier/{id}', [listClassController::class, 'updateList'])->name('list.miseajour');
 
 });
 Route::prefix('note')->group(function(){
-    Route::get('/noteView/{id}', [App\Http\controllers\noteController::class, 'noteView'])->name('note.view');
+    Route::get('/noteView/{id}', [noteController::class, 'noteView'])->name('note.view');
 
-    Route::get('/creeClasse/{id}', [App\Http\controllers\listClassController::class, 'listView'])->name('list.view');
-    Route::get('/checkList/{id}', [App\Http\controllers\listClassController::class, 'listCheck'])->name('list.check');
+    Route::get('/creeClasse/{id}', [listClassController::class, 'listView'])->name('list.view');
+    Route::get('/checkList/{id}', [listClassController::class, 'listCheck'])->name('list.check');
 
-    Route::post('/store/{id}', [App\Http\controllers\noteController::class, 'noteStore'])->name('note.store');
+    Route::post('/store/{id}', [noteController::class, 'noteStore'])->name('note.store');
 
     /* Route::get('/edit/{id}', [listClassController::class, 'editList'])->name('list.edit');
 

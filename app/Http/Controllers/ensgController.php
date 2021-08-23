14ca09b11f6409c8b26438be8a02593de049bfdb
unknown <imededin.ati@ensi-uma.tn>
2021-08-23 11:29:11 +0100

    conflict

--- app/Http/Controllers/ensgController.php
+++ app/Http/Controllers/ensgController.php
@@ -1,15 +1,15 @@
-  
 <?php
 
+namespace App\Http\Controllers;
 
 use App\Http\Controllers\Controller;
 
 use Illuminate\Http\Request;
 use App\Models\enseignant;
 
-class ensgController extends Controller
+class EnsgController extends Controller
 {
-
+    
     public function EnsView () {
         $data['allData'] = enseignant::all();
         return view('backend.ENS.view_Ens', $data);
@@ -29,59 +29,10 @@ class ensgController extends Controller
             'password'=>'required',
             'login' => 'required',
         ]);
-        $data = new enseignant();
-        $data->nom = $request->nom;
-        $data->prenom = $request->prenom;
-        $data->sexe = $request->sexe;
-        $data->password = bcrypt($request->password);
-        $data->login = $request->login;
-        $data->save();
+        enseignant::create($request->all());
+       
 
-
-        /* enseignant::create($request->all()); */
-
-        $notification = array(
-            'message' => 'تم إضافة المدرس بنجاح',
-            'alert-type' => 'success'
-        );
-
-
-        return back()->with($notification);
+        return back()->with('success',"enseignant ajouté avec succes");
     }
-
-
-
-    public function EnsUpdate(Request $request, $id) {
-
-        $data = enseignant::find($id);
-        $data->nom = $request->nom;
-        $data->prenom = $request->prenom;
-        $data->login = $request->login;
-        $data->sexe = $request->sexe;
-
-
-        $data->save();
-
-        $notification = array(
-            'message' => 'تم تحديث المدرس بنجاح',
-            'alert-type' => 'info'
-        );
-
-        return redirect()->route('Ens.view')->with($notification);
-    }
-
-
-
-    public function EnsDelete($id) {
-
-        $user = enseignant::find($id);
-        $user->delete();
-
-        $notification = array(
-            'message' => 'تم حذف المدرس بنجاح',
-            'alert-type' => 'info'
-        );
-
-        return redirect()->route('Ens.view')->with($notification);
-    }
+    
 }

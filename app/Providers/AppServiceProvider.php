2528a43e8052cc5b4dce5fc74773c90a75fc1319
Ibrahim Boubakri <ibrahim.boubakri@ensi-uma.tn>
2021-09-03 21:06:20 +0100

    Update AppServiceProvider.php

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -27,7 +27,7 @@ class AppServiceProvider extends ServiceProvider
     public function boot()
     {
         Schema::defaultStringLength(191);
-        $ecoleCreds = Ecole::firstWhere('id', 1);
-        view()->share('ecoleCreds', $ecoleCreds);
+        /* $ecoleCreds = Ecole::firstWhere('id', 1);
+        view()->share('ecoleCreds', $ecoleCreds); */
 
 }}
